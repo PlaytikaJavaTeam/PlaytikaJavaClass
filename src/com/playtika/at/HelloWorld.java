02c970be3fc1e4c780fdaa57d48830be820ccab3
ykogut <ykogut@playtika.com>
2016-05-26 13:24:12 +0300

    Hello world commit

---
+++ src/com/playtika/at/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.playtika.at;
+
+/**
+ * @author Kogut Yaroslav
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
