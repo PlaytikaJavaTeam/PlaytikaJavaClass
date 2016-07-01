cd6d2fe49fbfd44baf52ea4d9dcb6d4a54f39204
Nick <makarovmykola@gmail.com>
2016-07-01 15:21:42 +0300

    first inspection

--- src/com/playtika/at/LinkedListAlgoritm/Main.java
+++ src/com/playtika/at/LinkedListAlgoritm/Main.java
@@ -3,6 +3,7 @@ package com.playtika.at.LinkedListAlgoritm;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Исходные данные: Есть интовый линкедЛист, заполненый рендомно, придумать алгоритм наименьшей сложности
@@ -11,33 +12,33 @@ import java.util.List;
  * выбросить за пределы листа
  */
 public class Main {
+
     public static void main(String[] args) {
 
-        //Создаем линкедЛист и заполняем его рендомно в пределах 100
         List<Integer> randomList = new LinkedList<>();
-        for (int i = 0; i < 100 ; i++) {
+        for (int i = 0; i < 100; i++) {
             randomList.add(i);
         }
-        //перемешиваем все значения в листе
         Collections.shuffle(randomList);
         lastSummElement(randomList);
     }
-    //делаем метод который будет сумировать ключ и значение и результат будет след ключ
+
     private static void lastSummElement(List<Integer> randomList) {
 
-        //Заводим переменную счетчик кол-ва операций
+        Random randomGenerator = new Random();
         int counter = 0;
-        //Заводим переменную, которая хранит текущий ключ
-        int element = randomList.get(0);
 
-        //Выполняем действие пока не достигнем результата, при етом первое дейстие нужно выполнить обязательно
-        do {
-            //проверяем чтоб ключ не вывалился за пределы листа
-            //если выпал делим ключ пополам и пока не попадем в наш диапазон листа
+        for (int i = 0; i < randomList.size(); ) {
+            i += randomList.get(i);
+            counter++;
 
-            //если достигли крайнего елемента листа - выйти из цикла
-        } while (!(element == randomList.get(randomList.size() - 1)));
-        //напечатать в консоль кол-во операций
-        System.out.println(counter);
+            if (i > randomList.size() - 1) {
+                i = randomGenerator.nextInt(99);
+
+            } else if (i == 99) {
+                System.out.println(counter);
+            }
+        }
     }
 }
+
